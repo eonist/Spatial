3c794082c46a1f42f72a76ae2cc895fd62657af0
andre.jorgensen <andre.jorgensen@tidal.com>
2019-11-09 15:06:26 +0100

    Minor tweak

--- Spatial/src/align/aligntype/AlignType.swift
+++ Spatial/src/align/aligntype/AlignType.swift
@@ -3,12 +3,8 @@ import Foundation
  * Single align type
  * - Fixme: ⚠️️ Reference AlignType in HorAlign and VerAlign, and prob use CenterX,CenterY naming instead
  * - Note: Use .rawValue if you want to get the string "topLeft"
+ * - could also just do: no type enum and: Swift.print("\(String(describing: TestEnum.three))") // three
  */
 public enum AlignType: String {
-   case left
-   case right
-   case top
-   case bottom
-   case centerHor
-   case centerVer
+   case left, right, top, bottom, centerHor, centerVer
 }
