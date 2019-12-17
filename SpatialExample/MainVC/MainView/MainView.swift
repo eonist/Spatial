9998dc7d51ce5ee1f6fec614bf9b9e79700beefa
Eonist <30n1st@gmail.com>
2019-12-17 22:08:13 +0100

    Minor tweak

--- SpatialExample/MainVC/MainView/MainView.swift
+++ SpatialExample/MainVC/MainView/MainView.swift
@@ -2,10 +2,10 @@ import UIKit
 
 class MainView: UIView {
    lazy var cardView: CardView = createCardView()
-   lazy var spacingTestView: UIView = createSpacingTestView()
-   lazy var minMaxTestView: UIView = createMinMaxView()
-   lazy var testView: UIView = createTestView()
-   lazy var sizeTestingView: SizeTestingView = createSizeTestingView()
+//   lazy var spacingTestView: UIView = createSpacingTestView()
+//   lazy var minMaxTestView: UIView = createMinMaxView()
+//   lazy var testView: UIView = createTestView()
+//   lazy var sizeTestingView: SizeTestingView = createSizeTestingView()
    override init(frame: CGRect) {
       super.init(frame: frame)
       _ = cardView
