026e01d5a2f83681a830ad5d6141185489d07880
Eonist <30n1st@gmail.com>
2019-12-17 22:05:17 +0100

    Minor reorg

---
+++ Tests/Tests.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  SpatialExample
+//
+//  Created by Eon on 17/12/2019.
+//  Copyright © 2019 FutureLab. All rights reserved.
+//
+
+import Foundation
+
+//Align.align(someCircle, canvasSize: .init(x: 400, y: 300), canvasAlign: .center, objectAlign: .topLeft) // Output: centers a circle within 400x300 rectangle
+//   * Align.align(someCircle, canvasSize: .init(x: 400, y: 300), canvasAlign: .centerRight, objectAlign: .centerRight) // Output: aligns the circle to the y axis center and  to the right border of the rectangle but withinn the rectange
+//alignmentPoint
